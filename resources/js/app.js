5d91f389ce7c20f8bca195a7d6da0ba374395b54
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-09-10 15:06:21 +0600

    completed 2 sections of animation with locomotive and scrollTrigger and procxy scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,92 @@
 import "./bootstrap";
-import LocomotiveScroll from "locomotive-scroll";
+import gsap, {
+    Power2,
+    Power1,
+    Power3,
+    Power4,
+    Expo,
+    Back,
+    Elastic,
+    Bounce,
+    Rough,
+    Slow,
+    SteppedEase,
+    Circ,
+    Sine,
+} from "gsap";
+import ScrollTrigger from "gsap/src/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-const scroll = new LocomotiveScroll({
+const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main-body"),
     smooth: true,
+    lerp: 0.05,
 });
+
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+    scrollTop(value) {
+        return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    // pinType: document.querySelector(".smooth-scroll").style.transform
+    //     ? "transform"
+    //     : "fixed",
+});
+
+const timeline = gsap.timeline();
+timeline.to("#chinese", {
+    ease: Power2.easeOut,
+    duration: 1,
+    clipPath: "inset(0% 0% 0% 0%)",
+});
+
+timeline.to(
+    [
+        "#banner-intro h3",
+        "#banner-intro h1",
+        "#banner-intro p",
+        "#banner-intro button",
+    ],
+    {
+        ease: Expo.easeIn,
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        stagger: {
+            amount: 0.1,
+        },
+    }
+);
+
+timeline.to(".icon-box", {
+    ease: Expo.easeIn,
+    duration: 1,
+    x: 0,
+    opacity: 1,
+    scrollTrigger: {
+        trigger: "#expertise",
+        scroller: ".smooth-scroll",
+        scrub: true,
+    },
+    delay: 0.16,
+    stagger: {
+        amount: 0.4,
+    },
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shuvro Sarkar</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/locomotive.min.css') }}" rel="stylesheet" />
+
 </head>
 
-<body id="main-body" class="bg-primary-black  mx-auto lg:max-w-[1140px]" data - scroll - section>
+<body class="bg-primary-black  mx-auto lg:max-w-[1140px]" data-scroll-section>
     <header data - scroll - section
-        class="py-2 flex justify-between items-center max-h-20 absolute top-0 lg:min-w-[1140px] md:min-w-[920px] sm:min-w-full">
+        class="py-2 flex justify-between items-center max-h-20 absolute top-0 lg:min-w-[1140px] md:min-w-[920px] sm:min-w-full z-10">
         <img src="{{ asset('img/shuvro-logo.png') }}" alt="Logo" class="h-16 w-16">
         <nav class="navbar">
             <ul class="list-none flex justify-center items-center gap-10 text-light-white font-text text-base">
@@ -21,217 +23,228 @@
             </ul>
         </nav>
     </header>
-    <section class="pb-0 pt-15 w-full md:flex-row lg:flex-row flex-col " data - scroll - section>
-        <div class="lg:w-[39%] w-3/4">
-            <img src="{{ asset('img/women.jpg') }}" alt="Shuvro">
-        </div>
-        <div class="lg:w-[61%] ml-4 p-4 lg:p-0 lg:gap-3 gap-4 flex flex-col items-start">
-            <h3 class="text-primary-red">
-                Hello I 'm Shuvro</h3>
-            <h1> Visual Designer </h1>
-            <p class="lg:pr-14">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.Ut enim ad minim veniam. </p>
-            <button> About Me </button>
-        </div>
-    </section>
-    <section class="flex-col gap-32 half-bg w-full " data - scroll - section>
-        <div class=" flex justify-center items-center lg:gap-5 w-full gap-3 flex-wrap lg:flex-nowrap">
+    <main id="main-body">
+
+
+        <section class="pb-0 pt-15 w-full md:flex-row lg:flex-row flex-col h-screen" data-scroll-section>
+            <div class="lg:w-[39%] w-3/4">
+                <img src="{{ asset('img/women.jpg') }}" alt="Shuvro" id="chinese">
+            </div>
+            <div class="lg:w-[61%] ml-4 p-4 lg:p-0 lg:gap-3 gap-4 flex flex-col items-start" id="banner-intro">
+                <h3 class="text-primary-red">
+                    Hello I'm Shuvro</h3>
+                <h1> Visual Designer </h1>
+                <p class="lg:pr-14">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua.Ut enim ad minim veniam. </p>
+                <div class="button">
+                    <button class="mr-4"> About Me </button>
+                    <button>Contact Me</button>
+                </div>
+            </div>
+        </section>
+        <section class="flex-col gap-32 half-bg w-full " data-scroll-section>
             <div
-                class="p-10 bg-primary-background lg:w-1/3 md:w-1/3 flex gap-3 flex-col items-start w-[calc(50%-theme('gap.3'))]">
-                <img src="{{ asset('img/Pentablet.png') }}" width="60" height="60">
-                <h3>
-                    UI Design
-                </h3>
-                <p class="text-light-white">
-                    Lorem Ipsum is simply text of the printing type.
-                </p>
-                <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-3">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
+                class=" flex justify-center items-center lg:gap-5 w-full gap-3 flex-wrap lg:flex-nowrap "id="expertise">
+                <div
+                    class="p-10 bg-primary-background lg:w-1/3 md:w-1/3 flex gap-3 flex-col items-start w-[calc(50%-theme('gap.3'))] icon-box">
+                    <img src="{{ asset('img/Pentablet.png') }}" width="60" height="60">
+                    <h3>
+                        UI Design
+                    </h3>
+                    <p class="text-light-white">
+                        Lorem Ipsum is simply text of the printing type.
+                    </p>
+                    <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-3">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
+                </div>
+                <div
+                    class="p-10 bg-dark-red lg:w-1/3 flex gap-3 flex-col items-start md:w-1/3 w-[calc(50%-theme('gap.3'))] icon-box">
+                    <img src="{{ asset('img/Clipboard-Writing.png') }}" width="60" height="60">
+                    <h3>
+                        UI Design
+                    </h3>
+                    <p class="text-light-white">
+                        Lorem Ipsum is simply text of the printing type.
+                    </p>
+                    <button class="bg-transparent shadow-none font-semibold text-lg p-3">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow-white">
+                        </i>
+                    </button>
+                </div>
+                <div
+                    class="p-10 bg-primary-background lg:w-1/3 flex  gap-3 flex-col items-start md:w-1/3 w-[calc(50%-theme('gap.3'))] icon-box">
+                    <img src="{{ asset('img/Pencil-ruler.png') }}" width="60" height="60">
+                    <h3>
+                        UI Design
+                    </h3>
+                    <p class="text-light-white">
+                        Lorem Ipsum is simply text of the printing type.
+                    </p>
+                    <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-3">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
+                </div>
+            </div>
+            <div class=" flex justify-center items-center w-full">
+                <div class="m-2 w-1/3 flex flex-col justify-between items-center">
+                    <h1 class="text-light-red text-8xl mb-3">
+                        Tools
+                    </h1>
+                    <h3>
+                        i use every day
+                    </h3>
+                </div>
+                <div class="m-2 w-2/3 flex gap-4 flex-wrap justify-center items-center">
+                    <div class="box">
+                        <div class="flex justify-center items-center">
+                            <img src="{{ asset('img/wordpress.png') }}" alt="Wordpress" width="60" height="60">
+                            <img src="{{ asset('img/Elementor.png') }}" alt="Elementor" width="60" height="60">
+                            <img src="{{ asset('img/php.png') }}" alt="Elementor" width="60" height="60">
+                        </div>
+                        <p>
+                            Clients
+                        </p>
+                    </div>
+                    <div class="box ">
+                        <h3 class="text-light-red text-4xl">
+                            122
+                            +
+                        </h3>
+                        <p>
+                            Completed Projects
+                        </p>
+                    </div>
+                    <div class="box">
+                        <h3 class="text-light-red text-4xl">
+                            08
+                        </h3>
+                        <p>
+                            Years
+                            Experience
+                        </p>
+                    </div>
+                    <div class="box">
+                        <h3 class="text-light-red text-4xl">
+                            10
+                        </h3>
+                        <p>
+                            Achievements
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="flex-col gap-10" data-scroll-section>
+            <div class="w-full flex justify-between items-center px-3 lg:p-0">
+                <div class="w-4/6">
+                    <h3>
+                        FEATURED
+                        PROJECTS
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing eli
+                    </p>
+                </div>
+                <button class="w-2/6">
+                    VIEW
+                    ALL
                 </button>
             </div>
-            <div
-                class="p-10 bg-dark-red lg:w-1/3 flex gap-3 flex-col items-start md:w-1/3 w-[calc(50%-theme('gap.3'))]">
-                <img src="{{ asset('img/Clipboard-Writing.png') }}" width="60" height="60">
-                <h3>
-                    UI Design
-                </h3>
-                <p class="text-light-white">
-                    Lorem Ipsum is simply text of the printing type.
-                </p>
-                <button class="bg-transparent shadow-none font-semibold text-lg p-3">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow-white">
-                    </i>
-                </button>
-            </div>
-            <div
-                class="p-10 bg-primary-background lg:w-1/3 flex  gap-3 flex-col items-start md:w-1/3 w-[calc(50%-theme('gap.3'))]">
-                <img src="{{ asset('img/Pencil-ruler.png') }}" width="60" height="60">
-                <h3>
-                    UI Design
-                </h3>
-                <p class="text-light-white">
-                    Lorem Ipsum is simply text of the printing type.
-                </p>
-                <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-3">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
-                </button>
-            </div>
-        </div>
-        <div class=" flex justify-center items-center w-full">
-            <div class="m-2 w-1/3 flex flex-col justify-between items-center">
-                <h1 class="text-light-red text-8xl">
-                    12
-                </h1>
-                <h3>
-                    Years
-                    Experience
-                </h3>
-            </div>
-            <div class="m-2 w-2/3 flex gap-4 flex-wrap justify-center items-center">
-                <div class="box">
-                    <h3 class="text-light-red text-4xl">
-                        60 +
+            <div class="flex flex-wrap gap-7 justify-between lg:gap-12">
+                <div class="image-box">
+                    <img src="{{ asset('img/2-1024x853.jpg') }}" alt="">
+                    <h3 class="mt-3">
+                        The
+                        Vintage
                     </h3>
-                    <p>
-                        Clients
-                    </p>
+                    <button class="bg-transparent shadow-none mt-0 text-light-red font-semibold text-lg p-0">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
                 </div>
-                <div class="box ">
-                    <h3 class="text-light-red text-4xl">
-                        122
-                        +
+                <div class="image-box">
+                    <img src="{{ asset('img/3-1024x853.jpg') }}" alt="">
+                    <h3 class="mt-3">
+                        The
+                        Vintage
                     </h3>
-                    <p>
-                        Completed Projects
-                    </p>
+                    <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
                 </div>
-                <div class="box">
-                    <h3 class="text-light-red text-4xl">
-                        08
+                <div class="image-box">
+                    <img src="{{ asset('img/4-1024x853.jpg') }}" alt="">
+                    <h3 class="mt-3">
+                        The
+                        Vintage
                     </h3>
-                    <p>
-                        Years
-                        Experience
-                    </p>
+                    <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
                 </div>
-                <div class="box">
-                    <h3 class="text-light-red text-4xl">
-                        10
+                <div class="image-box">
+                    <img src="{{ asset('img/5-1024x853.jpg') }}" alt="">
+                    <h3 class="mt-3">
+                        The
+                        Vintage
                     </h3>
-                    <p>
-                        Achievements
-                    </p>
+                    <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
+                        KNOW
+                        MORE
+                        <i class="icon right-arrow">
+                        </i>
+                    </button>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="flex-col gap-10" data - scroll - section>
-        <div class="w-full flex justify-between items-center px-3 lg:p-0">
-            <div class="w-4/6">
-                <h3>
-                    FEATURED
-                    PROJECTS
-                </h3>
+        </section>
+        <section class="lg:p-20 bg-primary-background justify-between p-10 half-height-bg" data-scroll-section>
+            <div class="w-9/12">
+                <h2>
+                    LET’ S WORK TOGETHER ON YOUR NEXT PROJECT
+                </h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing eli
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                 </p>
             </div>
-            <button class="w-2/6">
-                VIEW
-                ALL
-            </button>
-        </div>
-        <div class="flex flex-wrap gap-7 justify-between lg:gap-12">
-            <div class="image-box">
-                <img src="{{ asset('img/2-1024x853.jpg') }}" alt="">
-                <h3 class="mt-3">
-                    The
-                    Vintage
-                </h3>
-                <button class="bg-transparent shadow-none mt-0 text-light-red font-semibold text-lg p-0">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
-                </button>
-            </div>
-            <div class="image-box">
-                <img src="{{ asset('img/3-1024x853.jpg') }}" alt="">
-                <h3 class="mt-3">
-                    The
-                    Vintage
-                </h3>
-                <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
-                </button>
-            </div>
-            <div class="image-box">
-                <img src="{{ asset('img/4-1024x853.jpg') }}" alt="">
-                <h3 class="mt-3">
-                    The
-                    Vintage
-                </h3>
-                <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
-                </button>
-            </div>
-            <div class="image-box">
-                <img src="{{ asset('img/5-1024x853.jpg') }}" alt="">
-                <h3 class="mt-3">
-                    The
-                    Vintage
-                </h3>
-                <button class="bg-transparent shadow-none text-light-red font-semibold text-lg p-0 mt-0">
-                    KNOW
-                    MORE
-                    <i class="icon right-arrow">
-                    </i>
-                </button>
-            </div>
-        </div>
-    </section>
-    <section class="lg:p-20 bg-primary-background justify-between p-10 half-height-bg" data - scroll - section>
-        <div class="w-9/12">
-            <h2>
-                LET’ S WORK TOGETHER ON YOUR NEXT PROJECT
-            </h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-            </p>
-        </div>
-        <button>
-            Contact
-        </button>
-    </section>
-    <section class="pb-11">
-        <ul class="list-none flex justify-center items-center gap-10 text-light-white font-text text-base w-full">
-            <li>
-                Home
-            </li>
-            <li>
-                Projects
-            </li>
-            <li>
+            <button>
                 Contact
-            </li>
-        </ul>
-    </section>
-    <footer class="min-h-[100px]  flex justify-between items-center relative px-5" data - scroll - section>
+            </button>
+        </section>
+        <section class="pb-11">
+            <ul class="list-none flex justify-center items-center gap-10 text-light-white font-text text-base w-full">
+                <li>
+                    Home
+                </li>
+                <li>
+                    Projects
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </section>
+    </main>
+    <footer class="min-h-[100px]  flex justify-between items-center relative px-5 bg-primary-background"
+        data-scroll-section>
         <img src="{{ asset('img/shuvro-logo.png') }}" class="h-16 w-16">
         <ul class="flex justify-center gap-8 items-center">
             <li>
@@ -261,7 +274,10 @@
         <p> Created by
             <span class="text-primary-red"> Noman </span>
         </p>
+
     </footer>
+    <script src={{ asset('js/locomotive.js') }}></script>
+    <script src={{ asset('js/app.js') }}></script>
 </body>
 
 </html>
